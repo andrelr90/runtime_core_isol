107544e0ef0b9d2aa74aeb929df23cea9cc34858
André Ribeiro <andrelr1999@hotmail.com>
2021-06-26 00:23:35 -0300

    test args

--- teste.c
+++ teste.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 #include <unistd.h>
-int main()
+int main(int argc, char *argv[])
 {
-    int amma = syscall(548, 2462);
+    int amma = syscall(548, atoi(argv[1]));
     printf("System call returned %d\n", amma);
     return 0;
 }
